6c9ca7d6f4a4e5837b95e4c6657b753de15cddda
kareem957 <s.karimulla350@gmail.com>
2022-10-23 17:10:00 +0530

    added java api's

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 
+const javaRoutes = require("./modules/java/routes");
 const pythonRoutes = require("./modules/python/routes");
 const javascriptRoutes = require("./modules/javascript/routes");
 
 const apiRouter = express.Router({ mergeParams: true });
 
-apiRouter.use("/python", pythonRoutes()).use("/java", javascriptRoutes()).use("/javascript", javascriptRoutes());
+apiRouter.use("/python", pythonRoutes()).use("/java", javaRoutes()).use("/javascript", javascriptRoutes());
 
 module.exports = apiRouter;
